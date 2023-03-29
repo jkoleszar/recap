f606ce8a908ab93f8041207803382e74c1b95894
John Koleszar <jkoleszar@gmail.com>
2023-03-29 11:03:40 -0400

    recap: initial skeleton
    Adds a REPL and a virtual machine for it to talk to.

---
+++ core/src/vm.rs
@@ -0,0 +1,23 @@
+use core::mem::MaybeUninit;
+use fixed_slice_vec::FixedSliceVec;
+
+#[derive(Clone, Copy, Debug)]
+pub enum MemoryCell {}
+
+pub struct Machine<'a> {
+    memory: FixedSliceVec<'a, MemoryCell>,
+    //stacks: FixedSliceVec<'a, FixedSliceVec<'a, MemoryCell>>,
+}
+
+impl<'a> Machine<'a> {
+    pub fn new(
+        memory: &'a mut [MaybeUninit<MemoryCell>],
+        //stacks: &'a mut [MaybeUninit<FixedSliceVec<'a, MemoryCell>>],
+    ) -> Self {
+        Self {
+            memory: FixedSliceVec::new(memory),
+            //stacks: FixedSliceVec::new(stacks),
+        }
+    }
+}
+
---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+pub use recap_core::*;
